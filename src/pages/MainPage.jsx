188b542dcba90c445d34f48085112e90ceefab02
InKyeongBae <inkkyung@gmail.com>
2022-06-22 22:42:08 +0900

    Feat : 아래에서 위로 fadeInUp 애니메이션

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,15 +4,15 @@ const Block1Style = styled.div`
 	@keyframes fadeInUp {
 		0% {
 			opacity: 0;
+		}
+		50% {
+			opacity: 0;
 			transform: translate3d(0, 100%, 0);
 		}
 		to {
 			opacity: 1;
 			transform: translateZ(0);
 		}
-	}
-	.container > div {
-		animation: fadeInUp 1s;
 	}
 
 	.block2 {
@@ -29,6 +29,7 @@ const Block1Style = styled.div`
 		);
 		border-radius: 15px;
 		color: white;
+		animation: fadeInUp 2s;
 	}
 
 	.block1 {
@@ -37,9 +38,9 @@ const Block1Style = styled.div`
 		height: 390px;
 		left: 781px;
 		top: 144px;
-
 		background: linear-gradient(61.62deg, #a9de97 26.7%, rgba(23, 122, 75, 0.83) 71.23%);
 		border-radius: 15px;
+		animation: fadeInUp 1.3s;
 	}
 
 	.block4 {
@@ -51,6 +52,7 @@ const Block1Style = styled.div`
 
 		background: linear-gradient(241.57deg, #7fb6e8 14.49%, rgba(34, 74, 215, 0.84) 78.86%);
 		border-radius: 15px;
+		animation: fadeInUp 4s;
 	}
 
 	.block3 {
@@ -62,6 +64,7 @@ const Block1Style = styled.div`
 
 		background: linear-gradient(109.97deg, #f3a9a4 15.03%, rgba(89, 94, 208, 0.56) 98.92%);
 		border-radius: 15px;
+		animation: fadeInUp 3s;
 	}
 
 	.fontstyle {
