78608a0b98229f6cd02682d76e6e3b9d238f10d2
InKyeongBae <inkkyung@gmail.com>
2022-06-22 19:36:17 +0900

    Refactor : ThemeProvider 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 
 import Mainpage from './Mainpage';
 import NotFound from './NotFound';
 
-const palette = {
-	// red: '#E75244',
-	// blue: '#367BBE',
-	// purple: '#8491E0',
-	// yellow: '#F9DE4B',
-	// green: '#5DCB83',
-	// white: '#ffffff',
-	black: '#000000',
-};
-
 function App() {
 	return (
 		<BrowserRouter>
-			<ThemeProvider theme={{ palette }}>
-				<Routes>
-					<Route
-						path='/'
-						element={<Mainpage />}
-					/>
-					<Route
-						path='*'
-						element={<NotFound />}
-					/>
-				</Routes>
-			</ThemeProvider>
+			<Routes>
+				<Route
+					path='/'
+					element={<Mainpage />}
+				/>
+				<Route
+					path='*'
+					element={<NotFound />}
+				/>
+			</Routes>
 		</BrowserRouter>
 	);
 }
--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Mainpage() {
 	return <div>Hello world</div>;
 }
