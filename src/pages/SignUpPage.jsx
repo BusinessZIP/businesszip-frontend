188ba5f206e467739d32818c1cfd64e7611d64f7
coreight98 <maxcha98@naver.com>
2022-06-23 00:28:26 +0900

    style : add style signIn and signUp

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,12 +3,64 @@ import styled from 'styled-components';
 
 import Input from '@/components/input';
 
-const Button = styled.button`
-	background: #0a3453;
+const Container = styled.div`
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+	background: linear-gradient(
+		69.01deg,
+		rgba(34, 74, 215, 0.18) 17.08%,
+		rgba(127, 182, 232, 0.85) 54.36%
+	);
+`;
+
+const FormWrapper = styled.div`
+	position: absolute;
+	width: 60%;
+	height: 80%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding: 5rem 3rem 3rem;
+	box-sizing: border-box;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	background: rgba(255, 255, 255, 0.5);
+	form {
+		height: 70%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	@supports ((-webkit-backdrop-filter: none) or (backdrop-filter: none)) {
+		backdrop-filter: blur(16px);
+	}
+	border-radius: 30px;
+`;
+
+const Title = styled.h2`
+	color: black;
+	font-size: 2rem;
+`;
+
+const SignUpInput = styled(Input)`
+	color: black;
+	font-size: 1.2rem;
+	::placeholder {
+		font-size: 1.2rem;
+		color: black;
+	}
+	height: 4rem;
+`;
+
+const SignInButton = styled.button`
+	background: #4365db;
+	border-radius: 20px;
 	color: white;
 	width: 100%;
 	border: none;
-	border-radius: 33px;
+	padding: 1.6rem 0px;
 `;
 
 function SignUpPage() {
@@ -27,50 +79,55 @@ function SignUpPage() {
 	};
 
 	return (
-		<div>
-			<h2>회원가입</h2>
-			<p>Log in</p>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Input
-					errorMessage={errors.name?.message}
-					placeholder='이름'
-					{...register('name', {
-						required: '이름을 입력해주세요',
-					})}
-				/>
-				<Input
-					errorMessage={errors.email?.message}
-					placeholder='이메일'
-					{...register('email', {
-						required: '이메일을 입력해주세요',
-						pattern: {
-							value: /^[A-Za-z0-9._%+-]+@[\S+.]+$/,
-							message: '이메일형식만 사용할 수 있습니다.',
-						},
-					})}
-				/>
-				<Input
-					errorMessage={errors.password?.message}
-					type='password'
-					placeholder='비밀번호'
-					{...register('password', {
-						required: '비밀번호를 입력해주세요.',
-					})}
-				/>
-				<Input
-					errorMessage={errors.phoneNumber?.message}
-					placeholder='전화번호'
-					{...register('phoneNumber', {
-						required: '전화번호를 입력해주세요',
-						pattern: {
-							value: /^([\d]{3}-[\d]{4}-[\d]{4}|[\d]{11})$/,
-							message: '올바른 전화번호가 아닙니다.',
-						},
-					})}
-				/>
-				<Button>회원가입</Button>
-			</form>
-		</div>
+		<Container>
+			<FormWrapper>
+				<Title>회원가입</Title>
+				<form>
+					<SignUpInput
+						errorMessage={errors.name?.message}
+						color='#ffffff'
+						placeholder='이름'
+						{...register('name', {
+							required: '이름을 입력해주세요',
+						})}
+					/>
+					<SignUpInput
+						errorMessage={errors.phoneNumber?.message}
+						color='#ffffff'
+						placeholder='전화번호'
+						{...register('phoneNumber', {
+							required: '전화번호를 입력해주세요',
+							pattern: {
+								value: /^([\d]{3}-[\d]{4}-[\d]{4}|[\d]{11})$/,
+								message: '올바른 전화번호가 아닙니다.',
+							},
+						})}
+					/>
+					<SignUpInput
+						errorMessage={errors.email?.message}
+						color='#ffffff'
+						placeholder='아이디'
+						{...register('email', {
+							required: '이메일을 입력해주세요',
+							pattern: {
+								value: /^[A-Za-z0-9._%+-]+@[\S+.]+$/,
+								message: '이메일형식만 사용할 수 있습니다.',
+							},
+						})}
+					/>
+					<SignUpInput
+						errorMessage={errors.password?.message}
+						color='#ffffff'
+						type='password'
+						placeholder='비밀번호'
+						{...register('password', {
+							required: '비밀번호를 입력해주세요.',
+						})}
+					/>
+				</form>
+				<SignInButton onClick={handleSubmit(onSubmit)}>가입하기</SignInButton>
+			</FormWrapper>
+		</Container>
 	);
 }
 
