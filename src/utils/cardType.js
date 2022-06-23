b650e3fc706e0d3f8ac3c224376ece97d464c7ea
coreight98 <maxcha98@naver.com>
2022-06-24 01:47:10 +0900

    feat : add card type maps

---
+++ src/utils/cardType.js
@@ -0,0 +1,13 @@
+export const CARD_TYPE_MAPS = {
+	A: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+001.png',
+
+	B: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+002.png',
+
+	C: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+003.png',
+
+	D: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+004.png',
+
+	E: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+005.png',
+
+	F: 'https://digging-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_Photo_2022-06-24-00-46-31+006.png',
+};
