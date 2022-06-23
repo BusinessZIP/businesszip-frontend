f624d5330149d5d605854d524780b526775d932f
coreight98 <maxcha98@naver.com>
2022-06-23 12:43:38 +0900

    feat : businessCard api

---
+++ src/app/api/businessCardApi.js
@@ -0,0 +1,30 @@
+import baseApi from '@/app/api/baseApi';
+
+const businessCardApi = baseApi
+	.enhanceEndpoints({
+		addTagTypes: ['Events'],
+	})
+	.injectEndpoints({
+		endpoints: (builder) => ({
+			getBusinessCards: builder.mutation({
+				query: (args) => ({
+					url: '/api/v1/bcard',
+					method: 'POST',
+					body: {
+						id: args.id,
+					},
+				}),
+			}),
+			getBusinessCardInfo: builder.mutation({
+				query: (args) => ({
+					url: '/api/v1/bcard/info',
+					method: 'POST',
+					body: {
+						id: args.id,
+					},
+				}),
+			}),
+		}),
+	});
+
+export default businessCardApi;
