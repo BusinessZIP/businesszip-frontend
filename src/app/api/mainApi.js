2b89b2ebb7ead9d449585b413ad5f479573faf5e
coreight98 <maxcha98@naver.com>
2022-06-23 12:43:51 +0900

    feat : auth api

---
+++ src/app/api/mainApi.js
@@ -0,0 +1,58 @@
+import { createSelector, createSlice } from '@reduxjs/toolkit';
+
+import baseApi from '@/app/api/baseApi';
+
+export const api = baseApi.enhanceEndpoints({}).injectEndpoints({
+	endpoints: (builder) => ({
+		login: builder.mutation({
+			query: (args) => ({
+				url: '/api/v1/login',
+				method: 'POST',
+				body: {
+					userId: args.email,
+					password: args.password,
+				},
+			}),
+		}),
+		signUp: builder.mutation({
+			query: (args) => ({
+				url: '/apiv1//signup',
+				method: 'POST',
+				body: {
+					userId: args.email,
+					password: args.password,
+					name: args.name,
+					phone: args.phoneNumber,
+				},
+			}),
+		}),
+		logout: builder.mutation({
+			queryFn: () => {
+				return {
+					data: undefined,
+				};
+			},
+		}),
+	}),
+});
+
+const initialAuthState = { token: null };
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState: initialAuthState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addMatcher(api.endpoints.login.matchFulfilled, (state, { payload }) => {
+				state.token = payload.token;
+			})
+			.addMatcher(api.endpoints.logout.matchFulfilled, () => {
+				return initialAuthState;
+			});
+	},
+});
+
+export const isLoginSelector = createSelector(
+	(state) => state.auth.token,
+	(token) => token !== null,
+);
